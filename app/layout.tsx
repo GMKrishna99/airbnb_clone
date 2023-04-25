2fd5171b85ff027c418389da247c7246351ca4dd
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-04-25 14:11:02 +0530

    chore : login and sign modal component added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
+import ClientOnly from './components/ClientOnly'
+import Modal from './components/Modal/Modal'
 import Navbar from './components/Navbar/Navbar'
 import './globals.css'
-import {Nunito} from 'next/font/google'
+import { Nunito } from 'next/font/google'
 export const metadata = {
   title: 'Airbnb',
   description: 'Airbnb Clone',
 }
 
 const font = Nunito({
-  subsets:["latin"]
+  subsets: ["latin"]
 })
 export default function RootLayout({
   children,
@@ -17,9 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
+        <ClientOnly>
+          <Modal title='hello, Airbnb' isOpen/>
+          <Navbar />
+
+        </ClientOnly>
         {children}
-        </body>
+      </body>
     </html>
   )
 }
